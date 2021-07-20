0bf3981edd8bc8b8d0fa2ca725301822b3c572c5
Hugo <blackfoxmmm@gmail.com>
2021-07-20 16:33:02 +0500

    Yeng

---
+++ app.js
@@ -0,0 +1,30 @@
+var elmr_fatiha = document.querySelector('#mr_fatiha'),
+    elmr_ikhlas = document.querySelector('#mr_ikhlas'),
+    elmr_falaq = document.querySelector('#mr_falaq'),
+    elaz_fatiha = document.querySelector('#az_fatiha'),
+    elaz_ikhlas = document.querySelector('#az_ikhlas'),
+    elaz_falaq = document.querySelector('#az_falaq'),
+    elaa_fatiha = document.querySelector('#aa_fatiha'),
+    elaa_ikhlas = document.querySelector('#aa_ikhlas'),
+    elaa_falaq = document.querySelector('#aa_falaq');
+
+   
+
+
+
+
+
+
+
+
+
+    // elForm.addEventListener('click', function(){
+    //     elAAU.play();
+    //     // elAud.pause();
+    //     // const music = new Audio(elAud);
+    //     // music.play();
+    //     // music.loop =true;
+    //     // music.playbackRate = 2;
+    //     // music.pause();qqazszdgfbgtyj
+    
+    // })
